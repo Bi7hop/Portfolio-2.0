7b5fb3bb962cf2f8b4c889949317e6e3757737ed
Marcel Menke <marcel.menke1981@gmail.com>
2025-01-30 10:28:58 +0100

    Navigation
    - added nav with styles / WIP

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+  currentLang = 'de';
+  isMenuOpen = false;
+
+  toggleLanguage() {
+    this.currentLang = this.currentLang === 'de' ? 'en' : 'de';
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    document.body.style.overflow = this.isMenuOpen ? 'hidden' : '';
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    document.body.style.overflow = '';
+  }
+}
