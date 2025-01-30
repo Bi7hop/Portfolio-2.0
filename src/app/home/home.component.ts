0834eaa9f287045797ebc948291fe9c422f5448c
Marcel Menke <marcel.menke1981@gmail.com>
2025-01-30 16:53:14 +0100

    Home
    - Changed styling
    - added angular material

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface TechIcon {
+  icon: string;
+  delay: number;
+  rotation?: number;
+  radius?: number;
+  speed?: number;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatIconModule, 
+    RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  techIcons: TechIcon[] = [
+    { icon: 'html', delay: 0 },
+    { icon: 'css', delay: 0 },
+    { icon: 'javascript', delay: 0 },
+    { icon: 'code', delay: 0 },
+    { icon: 'integration_instructions', delay: 0 },
+    { icon: 'developer_mode', delay: 0 },
+    { icon: 'devices', delay: 0 },
+    { icon: 'web', delay: 0 }
+  ];
+
+  ngOnInit() {
+    this.techIcons = this.techIcons.map((icon) => {
+      const radius = 300 + Math.random() * 400;
+      const rotation = Math.random() * 360;
+      const speed = Math.max(0.5, Math.random() + 0.5);
+      
+      return {
+        ...icon,
+        rotation,
+        radius,
+        speed, 
+        delay: 0
+      };
+    });
+  }
+}
